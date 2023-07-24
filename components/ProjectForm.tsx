7c5e4462bed34f828a88234910f02c03b1592595
Dev-scott <sadoscott25@gmail.com>
2023-07-24 05:16:58 +0100

    add post

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,30 +1,92 @@
 "use client";
 
-import { SessionInterface } from "@/common.type";
+import { FormState, ProjectInterface, SessionInterface } from "@/common.type";
 import { devNull } from "os";
-import React from "react";
+import React, { useState } from "react";
 import { ChangeEvent } from "react";
 import Image from "next/image";
 import FormField from "./FormField";
+import CustomMenu from "./CustomMenu";
+import { categoryFilters } from "@/constant";
+import { useRouter } from "next/navigation";
+import Button from "./Button";
+import { createNewProject, fetchToken } from "@/lib/action";
 
 type Props = {
   type: string;
   session: SessionInterface;
+  project?: ProjectInterface;
 };
+const ProjectForm = ({ type, session, project }: Props) => {
+  const router = useRouter();
 
-const ProjectForm = ({ type, session }: Props) => {
-  const handleFormSubmit = (e: React.FormEvent) => {};
+  const [submitting, setSubmitting] = useState<boolean>(false);
 
-  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {};
-  const handleStateChange = (fieldName:string , value : string ) =>{
+  const [form, setForm] = useState<FormState>({
+    title: project?.title || "",
+    description: project?.description || "",
+    image: project?.image || "",
+    liveSiteUrl: project?.liveSiteUrl || "",
+    githubUrl: project?.githubUrl || "",
+    category: project?.category || "",
+  });
 
+  const handleFormSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
+    setSubmitting(true);
 
-  }
+    const { token } = await fetchToken()
 
-  const form = {
-    image: "",
-    title:""
+    try {
+      if (type === "create") {
+        await createNewProject(form, session?.user?.id, token)
+
+        router.push("/");
+      }
+
+      if (type === "edit") {
+        // await updateProject(form, project?.id as string, token)
+
+        router.push("/");
+      }
+    } catch (error) {
+      alert(
+        `Failed to ${
+          type === "create" ? "create" : "edit"
+        } a project. Try again!`
+      );
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  const handleChangeImage = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    const file = e.target.files?.[0];
+
+    if (!file) return;
+
+    if (!file.type.includes("image")) {
+      alert("Please upload an image!");
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      const result = reader.result as string;
+
+      handleStateChange("image", result);
+    };
+  };
+
+  const handleStateChange = (fieldName: keyof FormState, value: string) => {
+    setForm((prevForm) => ({ ...prevForm, [fieldName]: value }));
   };
 
   return (
@@ -54,13 +116,55 @@ const ProjectForm = ({ type, session }: Props) => {
       </div>
 
       <FormField
-                title="Title"
-                state={form.title}
-                placeholder="Flexibble"
-                setState={(value) => handleStateChange('title', value)}
-            />
+        title="Title"
+        state={form.title}
+        placeholder="Flexibble"
+        setState={(value) => handleStateChange("title", value)}
+      />
 
+      <FormField
+        title="Description"
+        state={form.description}
+        placeholder="Showcase and discover remarkable developer projects."
+        isTextArea
+        setState={(value) => handleStateChange("description", value)}
+      />
 
+      <FormField
+        type="url"
+        title="Website URL"
+        state={form.liveSiteUrl}
+        placeholder="https://jsmastery.pro"
+        setState={(value) => handleStateChange("liveSiteUrl", value)}
+      />
+
+      <FormField
+        type="url"
+        title="GitHub URL"
+        state={form.githubUrl}
+        placeholder="https://github.com/adrianhajdin"
+        setState={(value) => handleStateChange("githubUrl", value)}
+      />
+
+      <CustomMenu
+        title="Category"
+        state={form.category}
+        filters={categoryFilters}
+        setState={(value) => handleStateChange("category", value)}
+      />
+
+      <div className="flexStart w-full">
+        <Button
+          title={
+            submitting
+              ? `${type === "create" ? "Creating" : "Editing"}`
+              : `${type === "create" ? "Create" : "Edit"}`
+          }
+          type="submit"
+          leftIcon={submitting ? "" : "/plus.svg"}
+          submitting={submitting}
+        />
+      </div>
     </form>
   );
 };
--- next.config.js
+++ next.config.js
@@ -2,8 +2,11 @@
 const nextConfig = {
 
     images:{
-        domains:['lh3.googleusercontent.com']
-    }
+        domains:['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
+      },
 
 }
 
