dca5cba464dccb761c8a9f6898b88de1e08770cc
Dev-scott <sadoscott25@gmail.com>
2023-07-26 03:55:31 +0100

    update

--- next.config.js
+++ next.config.js
@@ -4,9 +4,7 @@ const nextConfig = {
     images:{
         domains:['lh3.googleusercontent.com', 'res.cloudinary.com', 'task.com']
     },
-    experimental: {
-        serverComponentsExternalPackages: ['cloudinary', 'graphql-request']
-      },
+
       eslint: {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
